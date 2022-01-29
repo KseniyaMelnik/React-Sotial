3c487fb1ef5e450030faee09e028d6791b06d798
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-01-29 12:54:28 +0300

    feat: add useEffectf in component ProfileStatusWidthHooks

--- src/components/Profile/ProfileInfo/ProfileStatusWidthHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWidthHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 
 type ProfileStatusProps = {
     status: string
@@ -9,6 +9,10 @@ export const ProfileStatusWidthHooks  = (props: ProfileStatusProps) => {
 
     const [editMode, setEditMode] = useState<boolean>(false)
     const [status, setStatus] = useState<string>(props.status)
+
+    useEffect (()=>{
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true)
