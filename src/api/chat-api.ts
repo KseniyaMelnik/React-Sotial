7bfa4eaeecbbcd463bdc07788c6e59c5ea0f394f
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-03-14 18:50:45 +0300

    feat: add functions to chat-api

--- src/api/chat-api.ts
+++ src/api/chat-api.ts
@@ -1,14 +1,30 @@
+const subscribers = [] as Array<SubscriberType>
+
+let ws: WebSocket
+
+const closeHandler = () => {
+    setTimeout(createChannel, 3000)
+}
+function createChannel() {
+    if(ws){
+        ws.removeEventListener('close', closeHandler)
+        ws.close()
+    }
+    ws = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+    ws.addEventListener('close', closeHandler)
+}
+export const chatAPI = {
+    subscribe(callback: SubscriberType){
+        subscribers.push(callback)
+    }
+}
+
+type SubscriberType = (messages: ChatMessageType[])=> void
+
 export type ChatMessageType = {
     message: string
     photo: string
     userId: number
     userName: string
 
-}
-const subscribers = [] as Array<(messages: ChatMessageType[])=> void>
-
-export const chatAPI = {
-    subscribe(callback: (messages: ChatMessageType[])=> void){
-
-    }
 }
