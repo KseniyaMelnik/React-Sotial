378bd43b9303ff746d4599080432061a38ab1414
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-03-24 20:58:13 +0300

    refactor: add new file for UsersSearchForm component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {UserType} from "../../Redux/users-reducer";
 import Paginator from "../common/Paginator/Paginator";
 import User from "./User";
-import { Field, Form, Formik } from 'formik';
+import {UsersSearchForm} from "./UsersSearchForm";
 
 
 type UsersPropsType = {
@@ -35,39 +35,5 @@ const Users = (props: UsersPropsType) => {
         </div>
     </div>
 
-}
-const usersSearchFormValidate = (values:any) => {
-    const errors = {};
-    return errors;
-}
-type UsersSearchFormObjectType = {
-    term: string
-}
-const UsersSearchForm = ()=> {
-    const submit = (values: UsersSearchFormObjectType, { setSubmitting }:{setSubmitting: (isSubmitting: boolean) => void}) => {
-        setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-        }, 400);
-    }
-    return <div>
-        <Formik
-            initialValues={{ term: '' }}
-            validate={usersSearchFormValidate}
-            onSubmit={submit}
-        >
-            {({isSubmitting}) => (
-                <Form >
-                    <Field
-                        type="text"
-                        name="term"
-                    />
-                    <button type="submit" disabled={isSubmitting}>
-                        Find
-                    </button>
-                </Form>
-            )}
-        </Formik>
-    </div>
 }
 export default Users
---
+++ src/components/Users/UsersSearchForm.tsx
@@ -0,0 +1,34 @@
+import {Field, Form, Formik} from "formik";
+import React from "react";
+
+const usersSearchFormValidate = (values: any) => {
+    const errors = {};
+    return errors;
+}
+type UsersSearchFormObjectType = {
+    term: string
+}
+export const UsersSearchForm = () => {
+    const submit = (values: UsersSearchFormObjectType, {setSubmitting}: { setSubmitting: (isSubmitting: boolean) => void }) => {
+
+    }
+    return <div>
+        <Formik
+            initialValues={{term: ''}}
+            validate={usersSearchFormValidate}
+            onSubmit={submit}
+        >
+            {({isSubmitting}) => (
+                <Form>
+                    <Field
+                        type="text"
+                        name="term"
+                    />
+                    <button type="submit" disabled={isSubmitting}>
+                        Find
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    </div>
+}
