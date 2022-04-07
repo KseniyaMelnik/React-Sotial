a2b9a16ef38ccfbee518930793cb2b87436fdbcf
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-04-07 15:07:41 +0300

    fix: change erroneous display of status

--- src/components/Profile/ProfileInfo/ProfileStatusWidthHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWidthHooks.tsx
@@ -1,5 +1,8 @@
 import React, {useEffect, useState} from "react";
 import { Typography } from 'antd';
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../../Redux/redux-store";
+import {updateStatus} from "../../../Redux/profile-reducer";
 const { Paragraph } = Typography;
 
 
@@ -9,14 +12,21 @@ type ProfileStatusProps = {
 }
 
 export const ProfileStatusWidthHooks  = React.memo((props: ProfileStatusProps) => {
+    const dispatch = useDispatch()
+    const myId = useSelector<AppStateType, string|null>(state => state.auth.userId)
+    const userId = useSelector<AppStateType, number|undefined>( state=> state.profilePage.profile?.userId)
+    const isMyStatus = (myId == userId?.toString())
+    const status = useSelector<AppStateType, string>(state => state.profilePage.status)
 
-    const [status, setStatus] = useState<string>(props.status)
+    const [newStatus, setNewStatus] = useState<string>(status)
 
     useEffect(()=>{
-        props.updateStatus(status)
-    }, [status])
+        isMyStatus && dispatch(updateStatus(newStatus))
+    }, [newStatus])
+
 
     return <>
-        <Paragraph strong editable={{ onChange: setStatus }} style={{fontSize: 20, width: 250}}>{status}</Paragraph>
+        { !isMyStatus && <Paragraph strong editable={false} style={{fontSize: 20, width: 250}}>{status}</Paragraph>}
+        { isMyStatus && <Paragraph strong editable={{ onChange: setNewStatus }} style={{fontSize: 20, width: 250}}>{status}</Paragraph>}
     </>
 })
